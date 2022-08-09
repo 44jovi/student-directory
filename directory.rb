cdad12953707b1fe9c1be4fd599eab2213a0c811
jovi <kwong.jovi@gmail.com>
2022-08-09 12:12:39 +0100

    students as array of arrays

--- directory.rb
+++ directory.rb
@@ -1,23 +1,34 @@
-student_count = 11
 #let's put all students into an array
 students = [
-  "Dr. Hannibal Lecter",
-  "Darth Vader",
-  "Nurse Ratched",
-  "Michael Corleone",
-  "Alex DeLarge",
-  "The Wicked Witch of the West",
-  "Terminator",
-  "Freddy Krueger",
-  "The Joker",
-  "Joffrey Baratheon",
-  "Norman Bates"
+  ["Dr. Hannibal Lecter", :november],
+  ["Darth Vader", :november],
+  ["Nurse Ratched", :november],
+  ["Michael Corleone", :november],
+  ["Alex DeLarge", :november],
+  ["The Wicked Witch of the West", :november],
+  ["Terminator", :november],
+  ["Freddy Krueger", :november],
+  ["The Joker", :november],
+  ["Joffrey Baratheon", :november],
+  ["Norman Bates", :november]
 ]
-# and print them
-puts "The students of Villains Academy"
-puts "-------------"
-students.each do |student|
-  puts student
-end  
-# finally, we print the total number of students
-puts "Overall, we have #{students.count} great students"
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+
+def print(students)
+  students.each do |student|
+    puts "#{student[0]} (#{student[1]} cohort)"
+  end
+end
+
+def print_footer(names)
+  puts "Overall, we have #{names.length} great students"
+end
+
+# nothing happens until we call the methods
+print_header
+print(students)
+print_footer(students)
